e939e483b3ab41b290ffe285611b9278692e7c69
amrukwa <a.mrukwa00@gmail.com>
2022-10-16 12:07:45 +0200

    add model training

--- game/src/model.py
+++ game/src/model.py
@@ -1,5 +1,8 @@
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense, Flatten, Dropout, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.constraints import MaxNorm
+from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 import numpy as np
 
@@ -16,3 +19,26 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(
         X_train, y_train, test_size=0.125, 
         random_state=42, stratify=y_train)
+    
+    callback = EarlyStopping(monitor='val_accuracy', patience=10, verbose=True)
+    adam = Adam(learning_rate=0.001)
+
+    model = Sequential()
+    model.add(Dropout(0.2, input_shape=(X.shape[1],)))
+    model.add(Dense(100, activation="relu",
+        kernel_constraint=MaxNorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Dense(50, activation="relu", 
+        kernel_constraint=MaxNorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', 
+        optimizer=adam, metrics=['accuracy'])
+
+    history = model.fit(X_train, y_train,
+        validation_data=(X_val, y_val),
+        epochs=100, batch_size=16, callbacks=[callback],
+        verbose=1)
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+    model.save('game/model_file')
