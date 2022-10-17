5f4bae5ccb637fc5b20dc0c33e0d5a72fecab6e5
ArturOle <artur.oleksinski99@gmail.com>
2022-10-17 07:18:58 +0200

    Cards and integration with js

--- game/game.py
+++ game/game.py
@@ -3,18 +3,17 @@ from flask import Blueprint, render_template
 
 game_blueprint = Blueprint(
     name="game",
-    template_folder="./templates",
     static_folder="static",
-    static_url_path="/game/static",
+    static_url_path="/static",
     import_name=__name__
 )
 
 
 @game_blueprint.route("/game_bot", methods=["GET"])
 def game_bot():
-    return render_template("game/game.html")
+    return render_template("index.html")
 
 
 @game_blueprint.route("/game_dealer", methods=["GET"])
 def game_dealer():
-    return render_template("game/game.html")
+    return render_template("index.html")
